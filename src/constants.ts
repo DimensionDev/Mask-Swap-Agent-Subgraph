aa9015d65d64e962e1619621eeb9a5b4e0e70bf8
guanbinrui <guanbinrui@dimension.im>
2021-07-06 12:51:11 +0800

    :rocket: handle fill event

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const TOKEN_TYPE_ETHER = 0;
 export const TOKEN_TYPE_ERC20 = 1;
 
-export { ERC20 } from "../generated/SwapAgent/ERC20";
-export { ERC20NameBytes } from "../generated/SwapAgent/ERC20NameBytes";
-export { ERC20SymbolBytes } from "../generated/SwapAgent/ERC20SymbolBytes";
+export { ERC20 } from "../generated/SwapAgentETH/ERC20";
+export { ERC20NameBytes } from "../generated/SwapAgentETH/ERC20NameBytes";
+export { ERC20SymbolBytes } from "../generated/SwapAgentETH/ERC20SymbolBytes";
---
+++ src/mapping-bsc-mainnet.ts
@@ -0,0 +1,37 @@
+import {
+  SwapFilled,
+  SwapStarted,
+} from "../generated/SwapAgentBSC/SwapAgentBSC";
+import { Transaction } from "../generated/schema";
+import { fetchToken } from "./helpers";
+
+export function handleSwapStarted(event: SwapStarted): void {
+  // create token
+  let token = fetchToken(event.params.bep20Addr);
+  token.save();
+
+  // create transaction
+  let transaction = Transaction.load(event.transaction.hash.toHexString());
+  transaction.amount = event.params.amount;
+  transaction.fee = event.params.feeAmount;
+  transaction.address = event.params.fromAddr;
+  transaction.from_token = token.id;
+  transaction.created_by = event.transaction.hash;
+  transaction.created_at = event.block.timestamp;
+  transaction.save();
+}
+
+export function handleSwapFilled(event: SwapFilled): void {
+  // create token
+  let token = fetchToken(event.params.bep20Addr);
+  token.save();
+
+  // fill transaction
+  let transaction = Transaction.load(event.params.ethTxHash.toHex());
+  transaction.amount = event.params.amount;
+  transaction.address = event.params.toAddress;
+  transaction.to_token = token.id;
+  transaction.filled_by = event.transaction.hash;
+  transaction.filled_at = event.block.timestamp;
+  transaction.save();
+}
