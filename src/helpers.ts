f57406ad2bc479b47dc1eaad4740870b7ab77374
guanbinrui <guanbinrui@dimension.im>
2021-07-06 12:01:26 +0800

    :green_heart: generate different build for different networks

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,21 +1,27 @@
-import { Address } from '@graphprotocol/graph-ts'
-import { TOKEN_TYPE_ETHER, TOKEN_TYPE_ERC20, ERC20, ERC20NameBytes, ERC20SymbolBytes } from './constants'
-import { Token } from '../generated/schema'
+import { Address } from "@graphprotocol/graph-ts";
+import {
+  TOKEN_TYPE_ETHER,
+  TOKEN_TYPE_ERC20,
+  ERC20,
+  ERC20NameBytes,
+  ERC20SymbolBytes,
+} from "./constants";
+import { Token } from "../generated/schema";
 
 export function isETH(chain_id: number): boolean {
-  return chain_id == 1
+  return chain_id == 1;
 }
 
 export function isBSC(chain_id: number): boolean {
-  return chain_id == 56
+  return chain_id == 56;
 }
 
 export function isMatic(chain_id: number): boolean {
-  return chain_id == 137
+  return chain_id == 137;
 }
 
 export function isNative(value: string): boolean {
-  return value == '0x0000000000000000000000000000000000000000'
+  return value == "0x0000000000000000000000000000000000000000";
 }
 
 export function isNullEthValue(value: string): boolean {
@@ -26,80 +32,82 @@ export function isNullEthValue(value: string): boolean {
 }
 
 export function fetchToken(tokenAddress: Address): Token {
-  let token = Token.load(tokenAddress.toHexString())
+  let token = Token.load(tokenAddress.toHexString());
   if (token == null) {
-    token = new Token(tokenAddress.toHexString())
+    token = new Token(tokenAddress.toHexString());
   }
-  token.type = isNative(tokenAddress.toHex()) ? TOKEN_TYPE_ETHER : TOKEN_TYPE_ERC20
-  token.address = tokenAddress
-  token.name = fetchTokenName(tokenAddress)
-  token.symbol = fetchTokenSymbol(tokenAddress)
-  token.decimals = fetchTokenDecimals(tokenAddress)
-  return token as Token
+  token.type = isNative(tokenAddress.toHex())
+    ? TOKEN_TYPE_ETHER
+    : TOKEN_TYPE_ERC20;
+  token.address = tokenAddress;
+  token.name = fetchTokenName(tokenAddress);
+  token.symbol = fetchTokenSymbol(tokenAddress);
+  token.decimals = fetchTokenDecimals(tokenAddress);
+  return token as Token;
 }
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
   if (isNative(tokenAddress.toHexString())) {
-    return 'Native'
+    return "Native";
   }
 
-  let contract = ERC20.bind(tokenAddress)
-  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
+  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
 
   // try types string and bytes32 for symbol
-  let symbolValue = 'unknown'
-  let symbolResult = contract.try_symbol()
+  let symbolValue = "unknown";
+  let symbolResult = contract.try_symbol();
   if (symbolResult.reverted) {
-    let symbolResultBytes = contractSymbolBytes.try_symbol()
+    let symbolResultBytes = contractSymbolBytes.try_symbol();
     if (!symbolResultBytes.reverted) {
       // for broken pairs that have no symbol function exposed
       if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-        symbolValue = symbolResultBytes.value.toString()
+        symbolValue = symbolResultBytes.value.toString();
       }
     }
   } else {
-    symbolValue = symbolResult.value
+    symbolValue = symbolResult.value;
   }
 
-  return symbolValue
+  return symbolValue;
 }
 
 export function fetchTokenName(tokenAddress: Address): string {
   if (isNative(tokenAddress.toHexString())) {
-    return 'Native'
+    return "Native";
   }
 
-  let contract = ERC20.bind(tokenAddress)
-  let contractNameBytes = ERC20NameBytes.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
+  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
 
   // try types string and bytes32 for name
-  let nameValue = 'unknown'
-  let nameResult = contract.try_name()
+  let nameValue = "unknown";
+  let nameResult = contract.try_name();
   if (nameResult.reverted) {
-    let nameResultBytes = contractNameBytes.try_name()
+    let nameResultBytes = contractNameBytes.try_name();
     if (!nameResultBytes.reverted) {
       // for broken exchanges that have no name function exposed
       if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-        nameValue = nameResultBytes.value.toString()
+        nameValue = nameResultBytes.value.toString();
       }
     }
   } else {
-    nameValue = nameResult.value
+    nameValue = nameResult.value;
   }
 
-  return nameValue
+  return nameValue;
 }
 
 export function fetchTokenDecimals(tokenAddress: Address): i32 {
   if (isNative(tokenAddress.toHexString())) {
-    return 18
+    return 18;
   }
-  let contract = ERC20.bind(tokenAddress)
+  let contract = ERC20.bind(tokenAddress);
   // try types uint8 for decimals
-  let decimalValue = 0
-  let decimalResult = contract.try_decimals()
+  let decimalValue = 0;
+  let decimalResult = contract.try_decimals();
   if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value
+    decimalValue = decimalResult.value;
   }
-  return decimalValue
+  return decimalValue;
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -2,10 +2,10 @@ import {
   OwnershipTransferred,
   SwapFilled,
   SwapPairRegister,
-  SwapStarted
-} from "../generated/SwapAgent/SwapAgent"
-import { Transaction } from "../generated/schema"
-import { fetchToken } from "./helpers"
+  SwapStarted,
+} from "../generated/SwapAgent/SwapAgent";
+import { Transaction } from "../generated/schema";
+import { fetchToken } from "./helpers";
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 export function handleSwapFilled(event: SwapFilled): void {}
@@ -13,15 +13,15 @@ export function handleSwapPairRegister(event: SwapPairRegister): void {}
 
 export function handleSwapStarted(event: SwapStarted): void {
   // create token
-  let token = fetchToken(event.params.erc20Addr)
-  token.save()
+  let token = fetchToken(event.params.erc20Addr);
+  token.save();
 
   // create transaction
-  let id = `${event.transaction.hash.toHexString()}_${event.transaction.index}`
-  let transaction = Transaction.load(id)
-  transaction.amount = event.params.amount
-  transaction.fee = event.params.feeAmount
-  transaction.timestamp = event.block.timestamp
-  transaction.from = event.params.fromAddr
-  transaction.save()
+  let id = `${event.transaction.hash.toHexString()}_${event.transaction.index}`;
+  let transaction = Transaction.load(id);
+  transaction.amount = event.params.amount;
+  transaction.fee = event.params.feeAmount;
+  transaction.timestamp = event.block.timestamp;
+  transaction.from = event.params.fromAddr;
+  transaction.save();
 }
